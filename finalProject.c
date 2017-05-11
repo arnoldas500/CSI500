349b63fc00777adaf2623e2960a6e2f7173d4c11
arnoldas kurbanovas <akurbanovas@albany.edu>
2017-05-11 13:38:41 -0400

    updated working final version of finalProject.c

--- finalProject.c
+++ finalProject.c
@@ -1,3 +1,19 @@
+//Arnoldas Kurbanovas
+//ICSI500 final project
+//This program is run in a unix command line
+//by compiling using gcc finalProject.c
+//which creates an executable named a.out
+//to run the program after compiling the user needs to
+//specify which function they would like to run as the 
+//second argument: 
+//to see the forkProcess() function run: ./a.out fork
+//to see the orphanProcess() function run: ./a.out orphan
+//When you run the orphan process you will have to wait
+//up to 60 seconds after the program finished for the 
+//child process to finish and for it to print its PID#
+//as it is running independently of the parent and will
+//finish after control is given back to the user in the terminal
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
